c04350d00133d90814159af697f902a2cd8b7912
Cyrios <influx@influxdeMac-mini.local>
2024-02-22 16:06:17 +0800

    [Fix]: 补充Spin组件

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { Input, Button, message } from 'ant-design-vue'
+import { Input, Button, message, Spin } from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
-const components = [Input, Button]
+const components = [Input, Button, Spin]
 components.forEach((component) => {
   app.use(component)
 })
